b0da5814e3a73502997616e845fb39e1acd968d3
G <141026526+gref42@users.noreply.github.com>
2025-06-28 12:59:12 +0300

    Initial Commit

---
+++ AdvancedTaskTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedTaskTracker.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
