187e9b5c27408014131a4a44ebca6c9bcfc3909f
G <141026526+gref42@users.noreply.github.com>
2025-06-29 15:48:37 +0300

    Add Project model and CRUD views

---
+++ AdvancedTaskTracker/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AdvancedTaskTracker.Models;
+
+public class Project
+{
+    public int ProjectId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    
+    public List<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    
+    [BindNever]
+    public string? UserId { get; set; }
+    [BindNever]
+    public IdentityUser? User { get; set; }
+}
