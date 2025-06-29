7785bc7502b6a9811e6f43a858c51e2d5cda04a5
G <141026526+gref42@users.noreply.github.com>
2025-06-29 15:53:23 +0300

    Add ProjectsController with CRUD actions for user projects

---
+++ AdvancedTaskTracker/Controllers/ProjectsController.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdvancedTaskTracker.Data;
+using AdvancedTaskTracker.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdvancedTaskTracker.Controllers
+{
+    public class ProjectsController : BaseController
+    {
+
+        public ProjectsController(ApplicationDbContext context) : base(context) { }
+
+        // GET: Projects
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await FindUserProjectsAsync());
+        }
+
+        // GET: Projects/Details/5
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await FindUserProjectAsync(id.Value);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // GET: Projects/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projects/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProjectId,Name,Description")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                project.UserId = UserId;
+                _context.Add(project);
+                await _context.SaveChangesAsync();
+                
+                Console.WriteLine("Create finished");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await FindUserProjectAsync(id.Value);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProjectId,Name,Description")] Project editedProject)
+        {
+            if (id != editedProject.ProjectId)
+            {
+                return NotFound();
+            }
+            
+            var project = await FindUserProjectAsync(editedProject.ProjectId);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    project.Name = editedProject.Name;
+                    project.Description = editedProject.Description;
+                    
+                    
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(editedProject.ProjectId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(editedProject);
+        }
+
+        // GET: Projects/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await FindUserProjectAsync(id.Value);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var project = await FindUserProjectAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(p => p.ProjectId == id && p.UserId == UserId);
+        }
+
+        private async Task<Project?> FindUserProjectAsync(int id)
+        {
+            return await _context.Projects.FirstOrDefaultAsync(p => p.ProjectId == id && p.UserId == UserId);
+        }
+    }
+}
