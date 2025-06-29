814ab371d18a8cf3da50fe4b832af65310680fbd
G <141026526+gref42@users.noreply.github.com>
2025-06-29 15:54:38 +0300

    Add DbSet properties for TaskItem and Project in ApplicationDbContext

--- AdvancedTaskTracker/Data/ApplicationDbContext.cs
+++ AdvancedTaskTracker/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AdvancedTaskTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdvancedTaskTracker.Data;
@@ -9,4 +10,7 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<TaskItem> TaskItems { get; set; }
+    public DbSet<Project> Projects { get; set; }
 }
