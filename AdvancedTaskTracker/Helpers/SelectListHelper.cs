d332dcaaf1c07690e27e09a4256dce9a31cc656d
G <141026526+gref42@users.noreply.github.com>
2025-06-30 22:04:26 +0300

    Add SelectListHelper for generating enum select lists

---
+++ AdvancedTaskTracker/Helpers/SelectListHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdvancedTaskTracker.Helpers;
+
+public static class SelectListHelper
+{
+    public static SelectList GetEnumSelectList<T>(object? selected = null) where T : Enum
+    {
+        var values = Enum.GetValues(typeof(T)).Cast<T>();
+
+        var items = values.Select(value => new
+        {
+            Value = value,
+            Text = value.GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttributes(false)
+                .OfType<DisplayAttribute>()
+                .FirstOrDefault()?.Name ?? value.ToString()
+        });
+            
+        return new SelectList(items, "Value", "Text", selected);
+    }
+}
