26a229b985fa87040b435f1ffd4f0d93d6ddb545
G <141026526+gref42@users.noreply.github.com>
2025-06-30 22:05:12 +0300

    Add BaseController for shared functionality and user project retrieval

---
+++ AdvancedTaskTracker/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using AdvancedTaskTracker.Data;
+using AdvancedTaskTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedTaskTracker.Controllers;
+
+public class BaseController : Controller
+{
+    protected readonly ApplicationDbContext _context;
+
+    protected BaseController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    protected string UserId => User.FindFirstValue(ClaimTypes.NameIdentifier);
+    
+    protected async Task<List<Project>> GetUserProjectsAsync()
+    {
+        return await _context.Projects.Where(p => p.UserId == UserId).ToListAsync();
+    }
+
+}
