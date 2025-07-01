0c0f0431593b3f1e6179943414a120824d49f9e2
G <141026526+gref42@users.noreply.github.com>
2025-07-01 22:16:25 +0300

    Add display name for Due Date property in TaskItem

--- AdvancedTaskTracker/Models/TaskItem.cs
+++ AdvancedTaskTracker/Models/TaskItem.cs
@@ -26,6 +26,7 @@ public class TaskItem
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    [Display(Name="Due Date")]
     public DateTime? DueDate { get; set; }
     public Priority Priority { get; set; }
     public Status Status { get; set; }
